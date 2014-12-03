63a763d36fc5f9b1733bc8ab2ec468b8d9ea405e
Jacobo Tarrio <jtarrio@gmail.com>
2014-12-02 21:17:06 -0800

    Initial commit.

---
+++ MagstripeDecoder/Decoder.cs
@@ -0,0 +1,295 @@
+﻿using System;
+
+namespace MagstripeDecoder
+{
+    public enum DecoderState
+    {
+        NoCard, Sync, Decoding
+    }
+
+    public enum BitMode
+    {
+        Bits5, Bits7
+    }
+
+    public class ReceiveCharEventArgs : EventArgs
+    {
+        public char Chr { get; private set; }
+        public ReceiveCharEventArgs(char chr)
+        {
+            Chr = chr;
+        }
+    }
+
+    class Decoder
+    {
+        private PeakDetector peakDetector;
+        private BitDecoder bitDecoder;
+        private SymbolDecoder symbolDecoder;
+
+        public string Text { get { return symbolDecoder.Text; } }
+
+        public BitMode BitMode
+        {
+            get { return symbolDecoder.BitMode; }
+            set { symbolDecoder.BitMode = value; }
+        }
+
+        public Decoder(int sampleRate)
+        {
+            peakDetector = new PeakDetector(this);
+            bitDecoder = new BitDecoder(this, sampleRate);
+            symbolDecoder = new SymbolDecoder(this);
+            Clear();
+        }
+
+        public void Clear()
+        {
+            peakDetector.Clear();
+            bitDecoder.Clear();
+            symbolDecoder.Clear();
+        }
+
+        public void AddSamples(double[] samples)
+        {
+            peakDetector.AddSamples(samples);
+        }
+
+        private DecoderState State { get { return bitDecoder.State; } }
+
+        public delegate void ReceiveCharHandler(object sender, ReceiveCharEventArgs e);
+        public event ReceiveCharHandler ReceiveChar;
+
+        private void OnReceiveChar(char chr)
+        {
+            ReceiveChar(this, new ReceiveCharEventArgs(chr));
+        }
+
+        private class PeakDetector
+        {
+            private Decoder decoder;
+            private int counter;
+            private double noiseFloor;
+            private int edgeDirection;
+            private double highPeak;
+            private double lowPeak;
+            private double prevPeak;
+
+            public PeakDetector(Decoder decoder)
+            {
+                this.decoder = decoder;
+            }
+
+            public void Clear()
+            {
+                counter = 0;
+                noiseFloor = .05;
+                edgeDirection = 0;
+                highPeak = -1;
+                lowPeak = 1;
+                prevPeak = 0;
+            }
+
+            public void AddSamples(double[] samples)
+            {
+                for (int i = 0; i < samples.Length; ++i)
+                {
+                    var sample = samples[i];
+                    if (decoder.State == DecoderState.NoCard && sample > -2 * noiseFloor && sample < 2 * noiseFloor)
+                    {
+                        noiseFloor = (noiseFloor * 9999 + Math.Abs(sample)) / 10000;
+                    }
+                    else if (sample > 0)
+                    {
+                        highPeak = Math.Max(highPeak, sample);
+                        lowPeak = 1;
+                        if (sample < 0.9 * highPeak && highPeak > 0.25 * prevPeak && edgeDirection <= 0)
+                        {
+                            prevPeak = highPeak;
+                            decoder.bitDecoder.ReceivePeak(i + counter);
+                            edgeDirection = 1;
+                        }
+                    }
+                    else
+                    {
+                        lowPeak = Math.Min(lowPeak, sample);
+                        highPeak = -1;
+                        if (sample > 0.9 * lowPeak && lowPeak < -0.25 * prevPeak && edgeDirection >= 0)
+                        {
+                            prevPeak = -lowPeak;
+                            decoder.bitDecoder.ReceivePeak(i + counter);
+                            edgeDirection = -1;
+                        }
+                    }
+                }
+                counter += samples.Length;
+            }
+        }
+
+        private class BitDecoder
+        {
+            private Decoder decoder;
+            private int prevPeakSample;
+            private double clockSamples;
+            private int countedPeaks;
+            private bool currentBitIsOne;
+            private int stopSamples;
+
+            public DecoderState State { get; set; }
+
+            public BitDecoder(Decoder decoder, int sampleRate)
+            {
+                this.decoder = decoder;
+                this.stopSamples = (int)(0.150 * sampleRate);
+                Clear();
+            }
+
+            public void Clear()
+            {
+                State = DecoderState.NoCard;
+                prevPeakSample = -1;
+                clockSamples = -1;
+                countedPeaks = 0;
+                currentBitIsOne = false;
+            }
+
+            public void ReceivePeak(int sampleNum)
+            {
+                if (prevPeakSample < 0)
+                {
+                    prevPeakSample = sampleNum;
+                    return;
+                }
+                int samplesFromPrev = sampleNum - prevPeakSample;
+                if (samplesFromPrev > stopSamples || (State != DecoderState.NoCard && samplesFromPrev > 1.5 * clockSamples))
+                {
+                    decoder.symbolDecoder.Stop();
+                    State = DecoderState.NoCard;
+                    clockSamples = -1;
+                    countedPeaks = 0;
+                    prevPeakSample = sampleNum;
+                    return;
+                }
+                switch (State)
+                {
+                    case DecoderState.NoCard:
+                        if (clockSamples < 0)
+                        {
+                            clockSamples = samplesFromPrev;
+                            return;
+                        }
+                        clockSamples = (clockSamples * 3 + samplesFromPrev) / 4;
+                        prevPeakSample = sampleNum;
+                        ++countedPeaks;
+                        if (countedPeaks == 8)
+                        {
+                            State = DecoderState.Sync;
+                            currentBitIsOne = false;
+                        }
+                        return;
+                    case DecoderState.Sync:
+                        if (samplesFromPrev < 0.75 * clockSamples)
+                        {
+                            currentBitIsOne = true;
+                            State = DecoderState.Decoding;
+                        }
+                        else
+                        {
+                            currentBitIsOne = false;
+                            clockSamples = (clockSamples * 3 + samplesFromPrev) / 4;
+                            prevPeakSample = sampleNum;
+                        }
+                        return;
+                    case DecoderState.Decoding:
+                        if (samplesFromPrev < 0.75 * clockSamples)
+                        {
+                            currentBitIsOne = true;
+                            State = DecoderState.Decoding;
+                        }
+                        else
+                        {
+                            decoder.symbolDecoder.ReceiveBit(currentBitIsOne ? 1 : 0);
+                            currentBitIsOne = false;
+                            clockSamples = (clockSamples * 3 + samplesFromPrev) / 4;
+                            prevPeakSample = sampleNum;
+                        }
+                        return;
+                }
+            }
+        }
+
+        private class SymbolDecoder
+        {
+            private Decoder decoder;
+            private bool decoding = false;
+            private int bitsReceived = 0;
+            private int currentSymbol = 0;
+            private BitMode bitMode;
+            private int bits;
+            private int sentinel;
+            private int symbolMask;
+            private int byteMask;
+            private int asciiOffset;
+
+            public SymbolDecoder(Decoder decoder)
+            {
+                this.decoder = decoder;
+            }
+
+            public string Text { get; private set; }
+
+            public BitMode BitMode { get { return bitMode; } set { SetBitMode(value); } }
+
+            public void ReceiveBit(int bit)
+            {
+                currentSymbol = ((bit << (bits - 1)) | (currentSymbol >> 1)) & symbolMask;
+                if (!decoding && currentSymbol == sentinel)
+                {
+                    bitsReceived = 0;
+                    decoding = true;
+                }
+                if (decoding)
+                {
+                    if (bitsReceived == 0)
+                    {
+                        char chr = (char)((currentSymbol & byteMask) + asciiOffset);
+                        Text += Char.ConvertFromUtf32(chr);
+                        decoder.OnReceiveChar(chr);
+                    }
+                    bitsReceived = (bitsReceived + 1) % bits;
+                }
+            }
+
+            public void Stop()
+            {
+                decoding = false;
+                bitsReceived = 0;
+            }
+
+            public void Clear()
+            {
+                Stop();
+                Text = "";
+            }
+
+            private void SetBitMode(BitMode mode)
+            {
+                if (mode == BitMode.Bits5)
+                {
+                    sentinel = 0x0b;
+                    asciiOffset = 0x30;
+                    bits = 5;
+                }
+                else
+                {
+                    sentinel = 0x45;
+                    asciiOffset = 0x20;
+                    bits = 7;
+                }
+                symbolMask = (1 << bits) - 1;
+                byteMask = (1 << (bits - 1)) - 1;
+                bitMode = mode;
+            }
+        }
+    }
+}
---
+++ MagstripeDecoder/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Win32;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace MagstripeDecoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Decoder DecoderTrack1;
+        private Decoder DecoderTrack2;
+        private WasapiCapture Capture;
+
+        public MainWindow()
+        {
+            DecoderTrack1 = new Decoder(44100);
+            DecoderTrack1.BitMode = BitMode.Bits7;
+            DecoderTrack1.ReceiveChar += ReceiveCharacter;
+            DecoderTrack2 = new Decoder(44100);
+            DecoderTrack2.BitMode = BitMode.Bits5;
+            DecoderTrack2.ReceiveChar += ReceiveCharacter;
+            InitializeComponent();
+            PopulateDevices();
+        }
+
+        private void ReceiveCharacter(object sender, ReceiveCharEventArgs e)
+        {
+            if (sender == DecoderTrack1)
+            {
+                Track1Box.Text += Char.ConvertFromUtf32(e.Chr);
+            }
+            else
+            {
+                Track2Box.Text += Char.ConvertFromUtf32(e.Chr);
+            }
+        }
+
+        private void ReceiveWave(object sender, WaveInEventArgs e)
+        {
+            var samples = ParseStereo(e.Buffer, e.BytesRecorded);
+            Dispatcher.Invoke(new Action<double[]>(DecoderTrack1.AddSamples), samples.Item2);
+            Dispatcher.Invoke(new Action<double[]>(DecoderTrack2.AddSamples), samples.Item1);
+        }
+
+        private void PopulateDevices()
+        {
+            var enumerator = new MMDeviceEnumerator();
+            DeviceBox.ItemsSource = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            DeviceBox.SelectedIndex = 0;
+        }
+
+        private void StartRecording()
+        {
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+            LoadButton.IsEnabled = false;
+            Capture = new WasapiCapture((MMDevice)DeviceBox.SelectedItem);
+            Capture.WaveFormat = new WaveFormat(44100, 16, 2);
+            Capture.DataAvailable += ReceiveWave;
+            Capture.RecordingStopped += RecordingStopped;
+            Capture.StartRecording();
+        }
+
+        private void StopRecording()
+        {
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+            LoadButton.IsEnabled = true;
+            if (Capture != null)
+            {
+                Capture.DataAvailable -= ReceiveWave;
+                Capture.RecordingStopped -= RecordingStopped;
+                Capture.StopRecording();
+                Capture = null;
+            }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".wav";
+            dialog.Filter = "Sound files|*.wav";
+            var result = dialog.ShowDialog();
+            if (result == true)
+            {
+                var samples = LoadWaveFile(dialog.FileName);
+                DecoderTrack1.AddSamples(samples);
+                DecoderTrack2.AddSamples(samples);
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartRecording();
+        }
+
+        private void RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            StopRecording();
+            MessageBox.Show(e.Exception.ToString());
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopRecording();
+        }
+
+        private double[] LoadWaveFile(string filename)
+        {
+            byte[] data = File.ReadAllBytes(filename);
+            double[] samples = new double[(data.Length - 44) / 2];
+            for (int i = 0; i < samples.Length; ++i)
+            {
+                short num = (short)(data[44 + i * 2] | (data[45 + i * 2] << 8));
+                samples[i] = num / 32768.0;
+            }
+            return samples;
+        }
+
+        private Tuple<double[], double[]> ParseStereo(byte[] data, int len)
+        {
+            int numSamples = len / 4;
+            double[] leftSamples = new double[numSamples];
+            double[] rightSamples = new double[numSamples];
+            for (int i = 0; i < leftSamples.Length; ++i)
+            {
+                short num = (short)(data[i * 4] | (data[1 + i * 4] << 8));
+                leftSamples[i] = num / 32768.0;
+                num = (short)(data[2 + i * 4] | (data[3 + i * 4] << 8));
+                rightSamples[i] = num / 32768.0;
+            }
+            return new Tuple<double[], double[]>(leftSamples, rightSamples);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            DecoderTrack1.Clear();
+            DecoderTrack2.Clear();
+            Track1Box.Text = "";
+            Track2Box.Text = "";
+        }
+    }
+}
